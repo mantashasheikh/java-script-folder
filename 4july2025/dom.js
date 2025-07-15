a5d46df5b21802b12df4e09578e85f7d0aac7ee8
mantashasheikh <mantashasheikh95844@gmail.com>
2025-07-15 19:54:17 +0530

    dom in java script

--- 4july2025/dom.js
+++ 4july2025/dom.js
@@ -11,8 +11,22 @@ function Addtext(){
 
     let p = document.getElementById("para")
     p.innerHTML = "this is paragraph"
-    
+    p.style.color = "orange"
+    p.style.fontSize = "50px"
 
+    let s = document.getElementById("span")
+    s.innerHTML = "this is spam"
+    s.style.color = "blue"
+    s.style.fontSize = "50px"
+}
 
-    
+function green(){
+    let h = document.getElementById("heading")
+    h.style.color = "green"
+    h.style.fontSize = "100px"
+}
+function pink(){
+    let h = document.getElementById("heading")
+    h.style.color = "pink"
+    h.style.fontSize = "100px"
 }
