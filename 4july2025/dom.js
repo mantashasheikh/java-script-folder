6bde7b7a7978e256338127444126275da8432c98
mantashasheikh <mantashasheikh95844@gmail.com>
2025-07-15 12:34:02 +0530

    dom

---
+++ 4july2025/dom.js
@@ -0,0 +1,18 @@
+function Addtext(){
+    // let ok = document.getElementById("text")
+    //  let ok = document.querySelector("#text")
+
+    //  ok.innerHTML= "this is <i>javascript<i>"
+    // ok.innerText = "this is <i>javascript<i>"
+
+    // ok.style.color ="red"
+    // ok.style.backgroundColor = "lightblue"
+
+
+    let p = document.getElementById("para")
+    p.innerHTML = "this is paragraph"
+    
+
+
+    
+}
--- 4july2025/sript.js
+++ 4july2025/sript.js
@@ -60,24 +60,24 @@
         // while(n<=10)
 
 
-        var week = Number(prompt("enter a week number:"))
-        switch(week){
-            case 1: console.log("monday")
-            break
-            case 2: console.log("tuesday")
-            break
-            case 3: console.log("wednesday")
-            break
-            case 4: console.log("thursday")
-            break
-            case 5: console.log("friday")
-            break
-            case 6: console.log("saturday")
-            break
-            case 7: console.log("sunday")
-            break
-            default : console.log("invalid week")
-        }
+        // var week = Number(prompt("enter a week number:"))
+        // switch(week){
+        //     case 1: console.log("monday")
+        //     break
+        //     case 2: console.log("tuesday")
+        //     break
+        //     case 3: console.log("wednesday")
+        //     break
+        //     case 4: console.log("thursday")
+        //     break
+        //     case 5: console.log("friday")
+        //     break
+        //     case 6: console.log("saturday")
+        //     break
+        //     case 7: console.log("sunday")
+        //     break
+        //     default : console.log("invalid week")
+        // }
 
 
         // let price = Number(prompt("enter a price:"))
@@ -91,6 +91,8 @@
         // else{
         //     console.log("no discount your price is:", cost)
         // }
+
+        // 
 
         
 
