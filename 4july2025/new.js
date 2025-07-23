e334633ac681d6b8f2535141dce22bdd50dac0cc
mantashasheikh <mantashasheikh95844@gmail.com>
2025-07-23 20:15:23 +0530

    javascript new question

--- 4july2025/new.js
+++ 4july2025/new.js
@@ -16,3 +16,29 @@ function run(){
     }
 
 }
+
+
+let arr = [2,4,5,3,1]
+let max = 0
+for(let i=0 ; i<=arr.length ; i++){
+    if(arr[i]>max){
+        max=arr[i]
+    }
+}
+console.log("the maximum nuber in an array is",max)
+
+let num1 = 123456
+let ans = num1
+if(num1>0){
+    let rem = num1 % 10
+    let rev =  
+
+}
+ 
+
+
+
+
+
+
+
